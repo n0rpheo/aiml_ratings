abed4277c7120cb5e0872ed69adcff1ff456ddfc
Jan <janhendrik.abel@gmail.com>
2018-08-14 15:19:54 +0200

    First Commit

---
+++ src/ratings/get_affs.py
@@ -0,0 +1,18 @@
+import csv
+
+affiliations = set()
+with open("../../data/internal/affiliations.txt") as aff_file:
+
+    for line in aff_file:
+        affiliations.add(line.strip().lower())
+
+match_affs = set()
+with open("../../data/external/csratings.csv") as csratings_file:
+    spamreader = csv.DictReader(csratings_file, delimiter=',')
+
+    for idx, row in enumerate(spamreader):
+        if row['affiliation'].lower() in affiliations:
+            match_affs.add(row['affiliation'])
+
+print(str(match_affs))
+
